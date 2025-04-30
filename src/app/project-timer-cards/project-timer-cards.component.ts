9c1a1370cdc07d53ff22141af2694eaf81696551
SINJ <117652790+SINJ1@users.noreply.github.com>
2025-04-30 14:55:16 +0200

    Update project
    img+link

--- src/app/project-timer-cards/project-timer-cards.component.ts
+++ src/app/project-timer-cards/project-timer-cards.component.ts
@@ -17,17 +17,17 @@ interface Project {
 })
 export class ProjectTimerCardsComponent implements OnInit, OnDestroy {
   projects: Project[] = [
-    { title: 'Projet 1', image: 'assets/project/1.jpeg', link: 'https://www.youtube.com/watch?v=PCCx83zEns4&list=PLyJj6xkYbjteCAkSeTDPkiXm8KLmtgygB&index' },
-    { title: 'Projet 2', image: 'assets/project/2.jpeg', link: 'https://www.youtube.com/watch?v=VXnsLcvMp6Y&list=PLyJj6xkYbjteCAkSeTDPkiXm8KLmtgygB&index=' },
-    { title: 'Projet 3', image: 'assets/project/3.jpeg', link: 'https://www.youtube.com/watch?v=t2TvDJF7XRM&t' },
-    { title: 'Projet 4', image: 'assets/project/4.jpeg', link: 'https://www.facebook.com/reel/215446778094763' },
+    { title: 'Le Cinéma de la rue du Bac à Solde - Episode 5', image: 'assets/project/1.jpeg', link: 'https://www.youtube.com/watch?v=PCCx83zEns4&list=PLyJj6xkYbjteCAkSeTDPkiXm8KLmtgygB&index' },
+    { title: 'Le Cinéma de la rue du Bac à Solde - Episode 9', image: 'assets/project/2.jpeg', link: 'https://www.youtube.com/watch?v=VXnsLcvMp6Y&list=PLyJj6xkYbjteCAkSeTDPkiXm8KLmtgygB&index=' },
+    { title: 'Etoiles 2 Valenciennes la 8° édition', image: 'assets/project/3.jpeg', link: 'https://www.youtube.com/watch?v=t2TvDJF7XRM&t' },
+    { title: 'ILTV - LA TELE AU COEUR DU BASSIN MINIER', image: 'assets/project/4.jpeg', link: 'https://www.facebook.com/reel/215446778094763' },
     { title: 'Projet 5', image: 'assets/project/5.jpeg', link: 'https://www.facebook.com/CrossFitLXII/videos/406285128550176/' },
     { title: 'Projet 6', image: 'assets/project/6.jpeg', link: 'https://www.facebook.com/CrossFitLXII/videos/1441270116475895/' },
-    { title: 'Projet 7', image: 'assets/project/7.png', link: 'https://drive.google.com/file/d/1QzEws86bf5XdO2rXby77BiTAbtp0UdS4/view' },
-    { title: 'Projet 8', image: 'assets/project/8.png', link: 'https://drive.google.com/file/d/1fkK2Sl-qz27GA3GMGWlMyCXHq4bogQcD/view' },
-    { title: 'Projet 9', image: 'assets/project/9.png', link: 'https://drive.google.com/file/d/1WvJkmk1tevb7bbOphnj32myqP051lmvf/view' },
-    { title: 'Projet 10', image: 'assets/project/10.png', link: 'https://drive.google.com/file/d/1QLMhVlLGln7atYKXzus_FeolMetDQI7Q/view' },
-    { title: 'Projet 11', image: 'assets/project/11.png', link: 'https://drive.google.com/file/d/1P7QGLcKU2iiZ-pD6DAqtNyUcTWojQ5UY/view' },
+    { title: 'Générique de fin', image: 'assets/project/7.png', link: 'https://drive.google.com/file/d/1QzEws86bf5XdO2rXby77BiTAbtp0UdS4/view' },
+    { title: 'Générique de début', image: 'assets/project/8.png', link: 'https://drive.google.com/file/d/1fkK2Sl-qz27GA3GMGWlMyCXHq4bogQcD/view' },
+    { title: 'Le respect', image: 'assets/project/9.png', link: 'https://drive.google.com/file/d/1WvJkmk1tevb7bbOphnj32myqP051lmvf/view' },
+    { title: 'Travailler intelligemment', image: 'assets/project/10.png', link: 'https://drive.google.com/file/d/1QLMhVlLGln7atYKXzus_FeolMetDQI7Q/view' },
+    { title: 'VIDEOCLUB - Amour plastique', image: 'assets/project/11.png', link: 'https://drive.google.com/file/d/1P7QGLcKU2iiZ-pD6DAqtNyUcTWojQ5UY/view' },
   ];
 
   currentIndex = 0;
